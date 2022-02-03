692f417d7acf05200d2bcd59c6563d4e5fb1c36d
Jamison Burton <jamison@burtontalk.com>
2022-02-03 08:28:44 -0700

    add comments

--- MultiplicationTable/MultiplicationTable.vb
+++ MultiplicationTable/MultiplicationTable.vb
@@ -16,19 +16,19 @@ Module MultiplicationTable
         Dim countVert As Integer = 1
         Dim product As String = ""
 
-
+        'statement for the user to know what the table is multiplying
         Console.WriteLine("Enjoy you " & factor & " x " & factor & " Multiplication Table:" & vbNewLine)
-        For index = 1 To factor
-            For count = 1 To factor
-                countHori = count * countVert
+        For index = 1 To factor 'for loop that repeats until reaching the factor value
+            For count = 1 To factor 'nested for loop
+                countHori = count * countVert 'multiplies the count by the index of the first for loop
                 product = CStr(countHori)
-                Console.Write(product.PadRight(5))
+                Console.Write(product.PadRight(5)) 'displays the product and pads the space to the right for the next number
             Next
-            countVert = countVert + 1
-            Console.Write(vbNewLine)
+            countVert = countVert + 1 'increases the multiple by 1 for the next for loop
+            Console.Write(vbNewLine) 'Add a space for the next for loop
         Next
 
-            Console.ReadLine()
+        Console.ReadLine()
     End Sub
 
 End Module
